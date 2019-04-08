22715dc3dcc27679cd803af02387009daa108db7
ALI <semangatjuara2019@gmail.com>
2019-04-08 08:16:46 +0700

    perubahan ke 4.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
-var gulp=require('gulp');
-watch = require('gulp-watch');
+var gulp= require('gulp'),
+watch = require('gulp-watch'),
+postcss= require('gulp-postcss');
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested')
+cssImport=require('postcss-import');
 
 gulp.task('default',function(){
 	console.log("Hooray - you created a Gulp task ok");
@@ -11,7 +16,11 @@ gulp.task('html',function(){
 
 gulp.task('styles',function(){
 	console.log("Imagine Sass or PostCSS tasts running here");
-	return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles/styles.css'));
+	return gulp.src('./app/assets/styles/styles.css')
+	//.pipe(postcss([cssvars,autoprefixer]))
+	//.pipe(postcss([cssvars,nested,autoprefixer]))
+	.pipe(postcss([cssImport,cssvars,nested,autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch',function(){
